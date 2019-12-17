8c81f69b003f7e42d6879cf0bf27290e02d5a26a
Finlay Low <Finlay@dhcp-10-240-156-195.cp.wireless.private.cam.ac.uk>
2019-12-17 13:46:46 +0000

    Add Training model & Webcrawler & others

--- Train/train.py
+++ Train/train.py
@@ -1,6 +1,6 @@
 import argparse
 import datetime
-from Model import TheBox
+from Model import thebox
 import numpy as np
 import os
 import random
@@ -8,25 +8,39 @@ import sklearn.model_selection as ms
 import tensorflow as tf
 import time
 
+HEIGHT = 224
+WIDTH = 224
+CHANNELS = 3
+BATCH_SIZE = 5
+EPOCHS = 5
+
+CHECKPOINT_PATH = os.path.join("checkpoint","cp.ckpt")
+LOG_PATH = os.path.join("log","fit",datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
 def _get_img(filename, label):
     img = tf.io.read_file(filename)
-    img = tf.image.decode_jpeg(img)
+    img = tf.image.decode_jpeg(img, channels=CHANNELS)
     img = (tf.cast(img, tf.float32)/127.5) - 1
     img = tf.image.resize(img, (HEIGHT, WIDTH))
     return img, label
 
 def get_data(folder):
-    hot_files = [os.path.join(folder,CLASS_0,f) for f in os.listdir(os.path.join(folder,CLASS_0))]
-    random.shuffle(hot_files)
-    hot_labels = [0 for x in range(len(hot_files))]
-
-    not_files = [os.path.join(folder,CLASS_1,f) for f in os.listdir(os.path.join(folder,CLASS_1))]
-    random.shuffle(not_files)
-    not_labels = [1 for x in range(len(not_files))]
-
-    filenames = hot_files+not_files
-    labels = hot_labels+not_labels
-
+    labelss = [x for x in os.listdir(folder) if x[0] != '.']
+    print(labelss)
+    with open('labels.txt', 'w') as f:
+        for l in labelss:
+            f.write(l+'\n')
+    filenames = []
+    labels = []
+    index = 0
+    for label in labelss:
+        files = [os.path.join(folder,label,f) for f in os.listdir(os.path.join(folder,label)) if f[0] != '.']
+        random.shuffle(files)
+        lab = [index for x in range(len(files))]
+        index = index + 1
+        filenames.extend(files)
+        labels.extend(lab)
+    print(filenames, labels)
     train_filenames, val_filenames, train_labels, val_labels = ms.train_test_split(
                             filenames, labels, train_size=0.8)
     train_data = tf.data.Dataset.from_tensor_slices(
@@ -36,3 +50,42 @@ def get_data(folder):
     train_data = (train_data.map(_get_img).shuffle(buffer_size=1000).batch(BATCH_SIZE))
     val_data = (val_data.map(_get_img).shuffle(buffer_size=1000).batch(BATCH_SIZE))
     return train_data, val_data
+
+def train_model(model, folder):
+    print("Getting data")
+    train_data, val_data = get_data(folder)
+    print(train_data)
+
+    print("Training model")
+    #model.load_weights(CHECKPOINT_PATH)
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(CHECKPOINT_PATH,
+                                                monitor='val_loss',
+                                                verbose=0,
+                                                save_best_only=False,
+                                                save_weights_only=False,
+                                                mode='auto', save_freq=500)
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=LOG_PATH, histogram_freq=1, write_graph=True, write_images=True)
+    model.fit(x=train_data,
+            epochs=EPOCHS,
+            verbose=1,
+            validation_data=val_data,
+            callbacks=[checkpoint, tensorboard_callback])
+    tensorboard_callback.set_model(model)
+    model.save(os.path.join("model", "1"))
+
+def convert_tflite(model, folder):
+    print("Converting model")
+    converter = tf.lite.TFLiteConverter.from_saved_model("model/1/")
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    tflite_model = converter.convert()
+    open("converted_model_quantized.tflite", "wb").write(tflite_model)
+
+model = thebox.TheBox()
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+            metrics=['accuracy'])
+model.build(input_shape=(None, HEIGHT, WIDTH, CHANNELS))
+# To train:
+train_model(model, 'dataset')
+# To convert:
+convert_tflite(model, 'dataset')
